a80e123c6af31288766e42e6ce941f291593a508
dustisme <androsrivas@outlook.com>
2024-11-20 14:20:01 +0100

    finish while loop exercise

--- src/WhileLoops.java
+++ src/WhileLoops.java
@@ -1,17 +1,43 @@
+import java.util.Scanner;
+
 public class WhileLoops {
     public static void main(String[] args) {
 
-        //Escribe un bucle while que genere números aleatorios hasta que salga el número 7. Has de usar la función getRandomNumber
+        //Escribe un bucle while que genere números aleatorios hasta que salga el número 7.
+        //Has de usar la función getRandomNumber
+        int number = 0;
+
+        while ( number != 7) {
+            number = getRandomNumber(10);
+            System.out.println("Número generado: " + number);
+        }
+
+        //Escribe un bucle while que genere números aleatorios hasta que salga un número mayor a 70.
+        //Has de usar la función getRandomNumber
+        number = 0;
+
+        while (number < 70) {
+            number = getRandomNumber(70);
+            System.out.println("Número generado: " + number);
+        }
 
 
+        //Escribe un bucle do while que genere un número aleatorio entre 1 y 100.
+        //El usuario debe adivinar el número aleatorio y el programa debe seguir solicitando intentos
+        //hasta que el usuario adivine correctamente.
+        //Después de cada intento, el programa debe indicar si el número ingresado es mayor o menor
+        //que el número generado. Has de usar la función getRandomNumber
+        Scanner input = new Scanner(System.in);
 
-        //Escribe un bucle while que genere números aleatorios hasta que salga un número mayor a 70. Has de usar la función getRandomNumber
-        
+        int inputNumber = 0;
+        int generatedNumber = getRandomNumber(100);
 
+        do {
+            System.out.println("Adivina el número generado");
+            inputNumber = input.nextInt();
+        } while(inputNumber != generatedNumber);
 
-        //Escribe un bucle do while que genere un número aleatorio entre 1 y 100. El usuario debe adivinar el número aleatorio y el programa debe seguir solicitando intentos hasta que el usuario adivine correctamente. Después de cada intento, el programa debe indicar si el número ingresado es mayor o menor que el número generado. Has de usar la función getRandomNumber
-
-    
+        System.out.println("Muy bien. El número era: " + generatedNumber);
     }
 
     /**
